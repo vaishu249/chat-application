2cb0f4ddd7c79357d084a8ef29c749825be8f726
vaishu1109 <vaishu.sg1109@gmail.com>
2024-12-28 17:08:09 +0530

    design complete of search bar

--- Frontend/src/home/leftpart/Search.jsx
+++ Frontend/src/home/leftpart/Search.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import { FaSearch } from "react-icons/fa";
+
 
 function Search() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className='px-6 py-4'>
+            <form action=''>
+                <div className='flex space-x-3'>
+                <label className="border-[0.5px] border-gray-700 rounded-lg p-3 flex items-center gap-2  w-[80%] bg-slate-800">
+                    <input type="text" className="grow outline-none bg-slate-800" placeholder="Search" />
+                    
+                </label>
+                <button>
+                    <FaSearch className='text-5xl p-2 hover:bg-gray-600 rounded-full duration-300'/>
+                </button>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default Search
