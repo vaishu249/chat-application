35d749189d90e23065156689219ce33707077758
vaishu1109 <vaishu.sg1109@gmail.com>
2024-12-29 18:46:18 +0530

    Online status

--- Frontend/src/home/rightpart/Chatuser.jsx
+++ Frontend/src/home/rightpart/Chatuser.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 function Chatuser() {
   return (
-    <div>
-      chat user 
+    <div className='flex space-x-3 items-center justify-center bg-gray-900 hover:bg-gray-700 duration-300'>
+      <div className="avatar online">
+        <div className="w-16 rounded-full">
+          <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+        </div>
+      </div>
+      <div>
+        <h1 className='text-xl'>Vaishnavi</h1>
+        <span className='text-sm'>Offline</span>
+      </div>
     </div>
   )
 }
