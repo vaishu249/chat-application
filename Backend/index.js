3c50f6d4d8d12fad73bbd84d635e98e3a4259eee
vaishu1109 <vaishu.sg1109@gmail.com>
2024-12-30 19:13:23 +0530

    database connection

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import e from 'express';
 const app = express();
 
 dotenv.config();
-
 const PORT=process.env.PORT || 3001;
+
+const URI=process.env.MONGODB_URI;
+
+try {
+  mongoose.connect(URI)
+  console.log('Connected to MongoDB');
+} catch (error) {
+  console.log(error);
+}
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
